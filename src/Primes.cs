b001618d65639dceea7f9ba1deed26349beb50f4
Joe Rankin <rankinj@uww.edu>
2015-05-29 15:58:37 -0500

    Added Problem 35 solution. Added Primes utility.

---
+++ src/Primes.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace ProjectEuler {
+    public class Primes {
+
+        public static IList<int> Under(int max) {
+            BitArray PrimeBits = new BitArray(max, true);
+
+            PrimeBits.Set(0, false); // You don't actually need this, just
+            PrimeBits.Set(1, false); // remindig you that 2 is the smallest prime
+
+            int maxSqrt = (int) Math.Sqrt(max);
+            for(int P = 2; P < maxSqrt + 1; P++)
+                if(PrimeBits.Get(P))
+                    // These are going to be the multiples of P if it is a prime
+                    for(int PMultiply = P * 2; PMultiply < max; PMultiply += P)
+                        PrimeBits.Set(PMultiply, false);
+
+            // We use this to store the actual prime numbers
+            List<int> Primes = new List<int>();
+
+            for(int i = 2; i < max; i++)
+                if (PrimeBits.Get(i)) {
+                    Primes.Add(i);
+                }
+            return Primes;
+        }
+    }
+}
---
+++ src/Problem35.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ProjectEuler {
+    public class Problem35 {
+        public static void Main(string[] args) {
+            Console.WriteLine(Primes.Under(1000000).Count);
+
+            //var rotations = DigitRotations(197);
+            //foreach(var rotation in rotations) {
+            //    Console.WriteLine(String.Join(",", rotation));
+            //    Console.WriteLine(FromDigits(rotation));
+            //}
+
+            var circularPrimes = new List<int>();
+            for (var i = 0; i < TargetPrimes.Count; i++) {
+                var prime = TargetPrimes[i];
+                if(!circularPrimes.Contains(prime)) {
+                    var rotations = NumberRotations(prime);
+                    if (TargetPrimes.Intersect(rotations).Count() == rotations.Count()) {
+                        foreach (var num in rotations) {
+                            circularPrimes.Add(num);
+                            Console.WriteLine(num);
+                        }
+                    }
+                }
+            }
+
+            Console.WriteLine(circularPrimes.Count + " circular primes found");
+
+            Console.Read();
+        }
+
+        private static IList<int> TargetPrimes = Primes.Under(1000000);
+
+        static IList<int> Digits(int num) {
+            var digits = new List<int>();
+            while(num > 0) {
+                digits.Add(num % 10);
+                num = num / 10;
+            }
+            digits.Reverse();
+            return digits;
+        }
+
+        static IList<int[]> DigitRotations(int num) {
+            var digits = Digits(num);
+            var queue = new Queue<int>((List<int>)digits);
+            var rotations = new List<int[]>{ digits.ToArray()};
+            for (var i = 0; i < digits.Count-1; i++) {
+                queue.Enqueue(queue.Dequeue());
+                rotations.Add(queue.ToArray());
+            }
+
+            return rotations.ToList();
+        }
+
+        static int FromDigits(int[] digits) {
+            var num = 0;
+            for (var i = 0; i < digits.Count(); i++) {
+                num += digits[i]* (int)Math.Pow(10d, digits.Count()-1-i);
+            }
+            return num;
+        }
+
+        static IList<int> FromDigitsList(IList<int[]> digits) {
+            var numbers = new List<int>();
+            foreach (var num in digits) {
+                numbers.Add(FromDigits(num));
+            }
+            return numbers;
+        }
+
+        static IList<int> NumberRotations(int num) {
+            var digits = DigitRotations(num);
+            return FromDigitsList(digits).Distinct().ToList();
+        }
+    }
+}
